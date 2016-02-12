f9d0d3e1fa81c901c875687888a331350a50606c
ouzhigang <ouzhigangGBA@gmail.com>
2016-02-12 23:41:22 +0800

    Update BaseController.class.php

--- Application/Simple/Controller/BaseController.class.php
+++ Application/Simple/Controller/BaseController.class.php
@@ -45,7 +45,7 @@ class BaseController extends Controller {
 		}
 		
 		//公用部分
-		if(strpos($_SERVER["HTTP_REFERER"], "index.php") === false)
+		if(strpos($_SERVER["REQUEST_URI"], "index.php") === false)
 			$this->assign("admin_path", dirname(__APP__) . strtolower(MODULE_NAME) . "/" . strtolower(CONTROLLER_NAME));
 		else
 			$this->assign("admin_path", dirname(__APP__) . "/index.php/" . strtolower(MODULE_NAME) . "/" . strtolower(CONTROLLER_NAME));
